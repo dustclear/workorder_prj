b0f2b4d0f3dfe2036e56824b5a63d6a78bdbcd9c
dustclear <dustclear@163.com>
2015-01-08 09:42:02 +0800

    d

--- workorder_project/src/com/wos/common/WosHelper.java
+++ workorder_project/src/com/wos/common/WosHelper.java
@@ -18,7 +18,8 @@ public class WosHelper
 {
     private static final WosHelper _instance = new WosHelper();
     
-    private static final Gson _gson = new GsonBuilder().setDateFormat(WosConstant.DATE_TIME_FORMAT).create();
+    private static final Gson _gson = new GsonBuilder().setDateFormat(WosConstant.DATE_TIME_FORMAT)
+            .create();
     
     private WosHelper()
     {
@@ -26,7 +27,7 @@ public class WosHelper
     
     public static WosHelper getInstance()
     {
-        return _instance; 
+        return _instance;
     }
     
     public String getValueFromJsonText(String jsonText, String keyText)
@@ -34,10 +35,10 @@ public class WosHelper
         Gson gson = new Gson();
         Map<String, String> retMap = gson.fromJson(jsonText,
                 new TypeToken<Map<String, String>>()
-                {}.getType());
+                {
+                }.getType());
         return retMap.get(keyText);
     }
-    
     
     public String toJsonText(Object srcObj, Class srcClass)
     {
@@ -53,7 +54,7 @@ public class WosHelper
     
     public String gernerateInstallCode()
     {
-        return WosConstant.PREFIX_INSTALL_CODE+generateRadomStr(12);
+        return WosConstant.PREFIX_INSTALL_CODE + generateRadomStr(12);
     }
     
     public String generatePrimaryKey()
@@ -79,10 +80,14 @@ public class WosHelper
     public HttpServletRequest getRequest(WebServiceContext wsContext)
     {
         MessageContext mc;
-        mc = wsContext.getMessageContext();
-        
-        HttpServletRequest request = (HttpServletRequest)mc.get(AbstractHTTPDestination.HTTP_REQUEST);
-        
-        return request;
+        if (wsContext != null)
+        {
+            mc = wsContext.getMessageContext();
+            
+            HttpServletRequest request = (HttpServletRequest)mc.get(AbstractHTTPDestination.HTTP_REQUEST);
+            
+            return request;
+        }
+        return null;
     }
 }
